4d2323b3cb3be6f9277d8a185019ad39c25f4a43
BraeMay27 <braeden_mayer27@live.ca>
2021-02-25 13:38:00 -0700

    Update functions.php

--- further-ed/functions.php
+++ further-ed/functions.php
@@ -8,10 +8,12 @@
 function fe_enqueue_styles() {
     //styles
     wp_enqueue_style( 'reset', get_template_directory_uri() . '/assets/css/reset.css', false, '1.0', 'all');
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
     
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+    wp_enqueue_script( 'js-file', get_template_directory_uri() . '/assets/js/main.js');
     //scripts
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', array ( 'jquery' ), 1.1, true);
+    // wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', array ( 'jquery' ), 1.1, true);
 }
 add_action('wp_enqueue_scripts', 'fe_enqueue_styles');
 
