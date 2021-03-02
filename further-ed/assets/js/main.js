413a83ff9e4a0407d9ee02dcb4a0d36f17a10d96
BraeMay27 <braeden_mayer27@live.ca>
2021-03-02 13:11:41 -0700

    the last stuff i did

--- further-ed/assets/js/main.js
+++ further-ed/assets/js/main.js
@@ -1,26 +1,12 @@
-console.log('this is dumb');
+
 window.onload=function(){
     const clickButton = document.querySelector('.mobile-btn');
 
 clickButton.addEventListener('click', ()=>{
 document.querySelector('.main-menu').classList.toggle('show-nav');
-document.querySelector('.toggle-icon').classList.toggle("close");
 
 });
-  };
 
-// mobile toggle button
-// let mobileBtn = document.querySelector('.mobile-btn');
-// let nav = document.querySelector('.main-menu');
-
-// mobileBtn.addEventListener('click', () => {
-//     console.log('menu button has been clicked');
-    
-//     nav.classList.toggle('show-nav');
-// });
-
-
-// // the scroll to top button
 // let scrollBtn = document.querySelector('.scroll-btn');
 
 // window.onscroll = function() {scrollFunction()};
@@ -38,3 +24,18 @@ document.querySelector('.toggle-icon').classList.toggle("close");
 //     document.body.scrollTop = 0; // safari
 //     document.documentElement.scrollTop = 0; // chrome, firefox
 // }
+
+  };
+
+// mobile toggle button
+// let mobileBtn = document.querySelector('.mobile-btn');
+// let nav = document.querySelector('.main-menu');
+
+// mobileBtn.addEventListener('click', () => {
+//     console.log('menu button has been clicked');
+    
+//     nav.classList.toggle('show-nav');
+// });
+
+
+// // the scroll to top button
