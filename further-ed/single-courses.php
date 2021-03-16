839580e06e05c830ea6e081658ddbf8f751d650e
Cody <55256070+codyhandy@users.noreply.github.com>
2021-03-16 15:15:11 -0600

    Update single-courses.php

--- further-ed/single-courses.php
+++ further-ed/single-courses.php
@@ -27,7 +27,41 @@ get_header();
     </div>
 
     <div class="max-width">
+        <?php if($courseDesc = get_field( 'course-desc' )): ?>
+            <p><?php the_field('course-desc'); ?></p>
+        <?php endif; ?>
 
+        <?php if($instructor = get_field( 'course-instructor' )): ?>
+            <p><strong>Instructor:</strong> <?php the_field('course-instructor'); ?></p>
+        <?php endif; ?>
+        <?php if($location = get_field( 'course-location' )): ?>
+            <p><strong>Location:</strong> <?php the_field('course-location'); ?></p>
+        <?php endif; ?>
+        <?php if($courseNotification = get_field( 'course-notification' )): ?>
+            <p class="hi-lighted"><?php the_field('course-notification'); ?></p>
+        <?php endif; ?>
+
+        <div>
+            <div class="details">
+                <h2>Class Details</h2>
+
+                <!-- Espresso Plugin will go here -->
+
+                
+            </div>
+            <div class="venue">
+                <h2>Venue</h2>
+            </div>
+            <?php if($hasWaitlist = get_field( 'has-waitlist' )): ?>
+                <div class="waitlist">
+                    <?php if($waitlistText = get_field( 'waitlist-text' )): ?>
+                        <p><?php the_field('waitlist-text'); ?></p>
+                    <?php endif; ?>
+                    <p>If class is sold out - Please email here to be placed on the waitlist.</p>
+                    <button class="btn"><a href="#">Apply for waitlist</a></button>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
 
 </article>
