f91b5b3cf5e21f36a9d0d0dc42906f27082c8108
BraeMay27 <braeden_mayer27@live.ca>
2021-03-24 15:28:57 -0600

    welp i guess i didnt push
    hmm

--- further-ed/single-services.php
+++ further-ed/single-services.php
@@ -9,52 +9,56 @@
 */
 //display header
 get_header();
-?>                
+?>
 
-<?php if(have_posts()) : ?>
-<!-- start the loop -->
-<?php while(have_posts()) : the_post(); ?>
+<?php if (have_posts()) : ?>
+    <!-- start the loop -->
+    <?php while (have_posts()) : the_post(); ?>
 
-<!-- insert banner here -->
+        <!-- insert banner here -->
 
-<div class="banner" style="background: url(<?php $banner = get_field( 'banner-image' ); ?> <?php if($banner) {_e($banner);} ?>) no-repeat 50% 50%; background-size: cover;">
-    <div class="opacity">
-        <div class="banner-text max-width">
-            <h1><?php $bannerHeading = get_field( 'main-heading' ); ?> <?php if($bannerHeading) {_e($bannerHeading);} ?></h1>
-            <p><?php $bannerText = get_field( 'main-subheading' ); ?> <?php if($bannerText) {_e($bannerText);} ?></p>
+        <div class="banner" style="background: url(<?php echo z_taxonomy_image_url($category->$catID); ?>) no-repeat 50% 50%; background-size: cover;">
+            <div class="opacity">
+                <div class="banner-text max-width">
+                    <h1><?php $bannerHeading = get_field('main-heading'); ?> <?php if ($bannerHeading) {
+                                                                                    _e($bannerHeading);
+                                                                                } ?></h1>
+                    <p><?php $bannerText = get_field('main-subheading'); ?> <?php if ($bannerText) {
+                                                                                    _e($bannerText);
+                                                                                } ?></p>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-       
 
-<div class="service-container max-width lg-flex">
-    <div class="entry-content single-service">
-     <?php the_content(); ?>
-     </div>
-     
-    <div class = "form">
-        <?php 
-            
-            $form = get_field('contact_form')?>
-        
-            <?php if($form) :  ?> 
-    
-                <div class="contact-us-form"><?php the_field('contact_form'); ?></div>
-    
-            <?php endif; ?>
-        
+
+        <div class="service-container max-width lg-flex">
+            <div class="entry-content single-service">
+                <?php the_content(); ?>
+            </div>
+
+            <div class="form">
+                <?php
+
+                $form = get_field('contact_form') ?>
+
+                <?php if ($form) :  ?>
+
+                    <div class="contact-us-form"><?php the_field('contact_form'); ?></div>
+
+                <?php endif; ?>
+
+            </div>
         </div>
-</div>
- 
-  
- <?php endwhile; ?>
-        <!-- end while loop -->
-        <!-- this would be a good place to your pagination for individual article/blog posts -->
+
+
+    <?php endwhile; ?>
+    <!-- end while loop -->
+    <!-- this would be a good place to your pagination for individual article/blog posts -->
 <?php else : ?>
-        <!-- send to search page / some other general page with search function, tags, categories, archives,etc.. -->
-<?php get_template_part('template-parts/content', 'none'); ?>
+    <!-- send to search page / some other general page with search function, tags, categories, archives,etc.. -->
+    <?php get_template_part('template-parts/content', 'none'); ?>
 <?php endif; ?>
 
-    <!-- //out of the loop: this would be a good place to add sidebar to the right of the article/blog. -->
+<!-- //out of the loop: this would be a good place to add sidebar to the right of the article/blog. -->
 <!-- //display footer -->
 <?php get_footer(); ?>
