772c424fd13e401c6677045c2e53ebc38df29dd9
Cody <55256070+codyhandy@users.noreply.github.com>
2021-03-29 14:59:07 -0600

    checkout page and single course styling

--- further-ed/header.php
+++ further-ed/header.php
@@ -63,21 +63,15 @@
         </nav>
 
         <?php if (is_page()) : ?>
-            <div class="banner" style="background: url(<?php $banner = get_field('banner-image'); ?> <?php if ($banner) {
-                                                                                                            _e($banner);
-                                                                                                        } ?>) no-repeat 50% 50%; background-size: cover;">
+            <div class="banner" style="background: url(<?php $banner = get_field('banner-image'); ?> <?php if ($banner) { _e($banner); } ?>) no-repeat 50% 50%; background-size: cover;">
                 <div class="opacity">
                     <div class="banner-text max-width">
 
                         <div class="banner-logo" style="width:100px;"><?php $bannerLogo = get_field('logo'); ?> <?php if ($bannerLogo) : ?> <img src="<?php echo $bannerLogo ?>" /><?php endif; ?></div>
 
 
-                        <h1><?php $bannerHeading = get_field('main-heading'); ?> <?php if ($bannerHeading) {
-                                                                                        _e($bannerHeading);
-                                                                                    } ?></h1>
-                        <p><?php $bannerText = get_field('main-subheading'); ?> <?php if ($bannerText) {
-                                                                                    _e($bannerText);
-                                                                                } ?></p>
+                        <h1><?php $bannerHeading = get_field('main-heading'); ?> <?php if ($bannerHeading) { _e($bannerHeading); } ?></h1>
+                        <p><?php $bannerText = get_field('main-subheading'); ?> <?php if ($bannerText) { _e($bannerText); } ?></p>
 
                         <?php
                         $bannerLink = get_field('button');
