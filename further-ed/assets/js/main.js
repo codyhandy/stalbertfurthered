5d3f9ad9a03c84a840810ff955ae6ff57bed49c7
BraeMay27 <braeden_mayer27@live.ca>
2021-04-06 16:42:49 -0600

    scroll to top button
    added

--- further-ed/assets/js/main.js
+++ further-ed/assets/js/main.js
@@ -1,46 +1,52 @@
+window.onload = function () {
+  const clickButton = document.querySelector(".mobile-btn");
+  // const childButton = document.querySelector('.menu-item-276');
 
-window.onload=function(){
-    const clickButton = document.querySelector('.mobile-btn');
-    // const childButton = document.querySelector('.menu-item-276');
-
-
-  clickButton.addEventListener('click', ()=>{
-    document.querySelector('.main-menu').classList.toggle('show-nav');
-    clickButton.classList.toggle('open');
-    
+  clickButton.addEventListener("click", () => {
+    document.querySelector(".main-menu").classList.toggle("show-nav");
+    clickButton.classList.toggle("open");
   });
 
   var $j = jQuery.noConflict();
- $j(function() {
-    $j('ul li').click(function() {
-        $j(this).siblings().removeClass('active');
-        $j(this).toggleClass('active');
-    })
-})
- 
-     
+  $j(function () {
+    $j("ul li").click(function () {
+      $j(this).siblings().removeClass("active");
+      $j(this).toggleClass("active");
+    });
+  });
 
- 
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-// let scrollBtn = document.querySelector('.scroll-btn');
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  var rootElement = document.documentElement;
 
-// window.onscroll = function() {scrollFunction()};
+  function scrollToTop() {
+    // Scroll to top logic
+    rootElement.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+  scrollToTopBtn.addEventListener("click", scrollToTop);
 
-// function scrollFunction () {
-//     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-//         scrollBtn.style.display = 'block';
-//     } else {
-//         scrollBtn.style.display = 'none';
-//     }
-// }
+  // let scrollBtn = document.querySelector('.scroll-btn');
 
-// // scrolls to the top when the button is pressed
-// function topFunction() {
-//     document.body.scrollTop = 0; // safari
-//     document.documentElement.scrollTop = 0; // chrome, firefox
-// }
+  // window.onscroll = function() {scrollFunction()};
 
-  };
+  // function scrollFunction () {
+  //     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  //         scrollBtn.style.display = 'block';
+  //     } else {
+  //         scrollBtn.style.display = 'none';
+  //     }
+  // }
+
+  // // scrolls to the top when the button is pressed
+  // function topFunction() {
+  //     document.body.scrollTop = 0; // safari
+  //     document.documentElement.scrollTop = 0; // chrome, firefox
+  // }
+};
 
 // mobile toggle button
 // let mobileBtn = document.querySelector('.mobile-btn');
@@ -48,9 +54,8 @@ window.onload=function(){
 
 // mobileBtn.addEventListener('click', () => {
 //     console.log('menu button has been clicked');
-    
+
 //     nav.classList.toggle('show-nav');
 // });
-
 
 // // the scroll to top button
--- further-ed/footer.php
+++ further-ed/footer.php
@@ -1,20 +1,24 @@
 <?php
+
 /**
-* The footer for our theme
-* Contains the closing tag for the main container section (i.e. <div class="main-content") and all of the
-*footer content.
-*
-*@package further-ed
-* @since 1.0.0
-*/
+ * The footer for our theme
+ * Contains the closing tag for the main container section (i.e. <div class="main-content") and all of the
+ *footer content.
+ *
+ *@package further-ed
+ * @since 1.0.0
+ */
 ?>
 </div>
- <!-- closing tag for site-content"> -->
+<!-- closing tag for site-content"> -->
 <footer>
 
-<?php get_template_part( 'template-parts/sidebar', 'footer' ); ?>
+    <?php get_template_part('template-parts/sidebar', 'footer'); ?>
+
+    <button id="scrollToTopBtn"><i class="far fa-arrow-alt-circle-up"></i></button>
 
 
 </footer>
 </body>
+
 </html>
