4fe7f1d0f8205f7b26f595be4763d9461e2ad48e
Cody <55256070+codyhandy@users.noreply.github.com>
2021-04-09 16:14:52 -0600

    rearranged homepage to add news section and clean up interface

--- further-ed/template-parts/content-home.php
+++ further-ed/template-parts/content-home.php
@@ -51,40 +51,81 @@
 
     <section class="section1">
         <div class="max-width lg-flex">
-            <div class="max-width">
-                <h2><?php $headingFeatured = get_field('heading-featured'); ?> <?php if ($headingFeatured) {
-                                                                                    _e($headingFeatured);
-                                                                                } ?></h2>
-                <p><?php $textFeatured = get_field('text-featured'); ?> <?php if ($textFeatured) {
-                                                                            _e($textFeatured);
-                                                                        } ?></p>
+            <div>
+                <h2><?php $headingSection1 = get_field('heading-section1'); ?> <?php if ($headingSection1) { _e($headingSection1); } ?></h2>
+                <p><?php $textSection1 = get_field('text-section1'); ?> <?php if ($textSection1) { _e($textSection1); } ?></p>
 
                 <!-- this block is for dynamically loading links  -->
                 <?php
-                $linkFeatured = get_field('link-featured');
-                if ($linkFeatured) :
+                $linkSection1 = get_field('link-section1');
+                if ($linkSection1) :
                     // create variables to store the link and the title
-                    $link_title = $linkFeatured['title'];
-                    $link_url = $linkFeatured['url'];
+                    $link_title = $linkSection1['title'];
+                    $link_url = $linkSection1['url'];
                 ?>
                     <button class="btn"><a href="<?php print_r(esc_url($link_url)); ?>"><?php print_r(esc_html($link_title)); ?></a></button>
                 <?php endif; ?>
+
+
             </div>
             <?php if ($imageSection1 = get_field('image-section1')) : ?>
                 <div class="image-container mobile-hidden max-width">
-                    <img src="<?php if ($imageSection1) {
-                                    _e($imageSection1);
-                                } ?>" alt="">
+                    <img src="<?php if ($imageSection1) {  _e($imageSection1); } ?>" alt="">
                 </div>
             <?php endif; ?>
         </div> <!-- end of the max width wrapper -->
     </section>
 
+    <section class="featured news">
+        <div class="text-container max-width">
+            
+
+            <!-- only adds an image to this sections if one has been chosen -->
+            <?php $imageFeatured = get_field('image-featured'); ?>
+            <?php if($imageFeatured): ?>
+            <div class="lg-flex">
+                <div class="image-container mobile-hidden max-width">
+                    <img src="<?php if ($imageFeatured) {  _e($imageFeatured); } ?>" alt="">
+                </div>
+            <?php endif; ?>
+
+            <div class="text-container">
+                <h2><?php $headingFeatured = get_field('heading-featured'); ?> <?php if ($headingFeatured) { _e($headingFeatured); } ?></h2>
+                <p><?php $textFeatured = get_field('text-featured'); ?> <?php if ($textFeatured) { _e($textFeatured); } ?></p>
+
+                <!-- this block is for dynamically loading links  -->
+                <?php
+                    $linkFeatured = get_field('link-featured');
+                    if ($linkFeatured) :
+                        // create variables to store the link and the title
+                        $link_title = $linkFeatured['title'];
+                        $link_url = $linkFeatured['url'];
+                    ?>
+                        <button class="btn inv"><a href="<?php print_r(esc_url($link_url)); ?>"><?php print_r(esc_html($link_title)); ?></a></button>
+                <?php endif; ?>
+
+                <p><?php $textFeatured2 = get_field('text-featured-2'); ?> <?php if ($textFeatured2) { _e($textFeatured2); } ?></p>
+
+                <?php
+                    $linkFeatured2 = get_field('link-featured-2');
+                    if ($linkFeatured2) :
+                        // create variables to store the link and the title
+                        $link_title = $linkFeatured2['title'];
+                        $link_url = $linkFeatured2['url'];
+                    ?>
+                        <button class="btn inv"><a href="<?php print_r(esc_url($link_url)); ?>"><?php print_r(esc_html($link_title)); ?></a></button>
+                <?php endif; ?>
+            </div>
+
+            <?php if($imageFeatured): ?>
+            </div> <!-- end of the flex container -->
+            <?php endif; ?>
+        </div>
+    </section>
+
     <section>
         <div class="max-width">
-            <h2><?php $headingServices = get_field('heading-services'); ?> <?php if ($headingServices) {
-                                                                                _e($headingServices);
-                                                                            } ?></h2>
+            <h2><?php $headingServices = get_field('heading-services'); ?> <?php if ($headingServices) { _e($headingServices); } ?></h2>
 
             <!-- loops through and displays posts from the "services categories" -->
             <?php if ($the_service_query->have_posts()) : ?>
@@ -94,9 +135,7 @@
                             <?php if ($serviceIcon = get_field('service-icon')) : ?>
                                 <div class="icon"><?php the_field('service-icon'); ?></div>
                             <?php endif; ?>
-                            <h3><?php $serviceHeading = get_field('service-heading'); ?> <?php if ($serviceHeading) {
-                                                                                                _e($serviceHeading);
-                                                                                            } ?></h3>
+                            <h3><?php $serviceHeading = get_field('service-heading'); ?> <?php if ($serviceHeading) {  _e($serviceHeading); } ?></h3>
                             <button class="btn"><a href="<?php the_permalink(); ?>">View More</a></button>
                         </div>
                     <?php endwhile; ?>
@@ -106,33 +145,27 @@
         </div>
     </section>
 
-    <section class="featured">
+    <section class="featured about">
         <div class="text-container max-width">
-            <h2><?php $headingSection1 = get_field('heading-section1'); ?> <?php if ($headingSection1) {
-                                                                                _e($headingSection1);
-                                                                            } ?></h2>
-            <p><?php $textSection1 = get_field('text-section1'); ?> <?php if ($textSection1) {
-                                                                        _e($textSection1);
-                                                                    } ?></p>
+            <h2><?php $headingAbout = get_field('heading-about'); ?> <?php if ($headingAbout) { _e($headingAbout); } ?></h2>
+            <p><?php $textAbout = get_field('text-about'); ?> <?php if ($textAbout) { _e($textAbout); } ?></p>
 
             <!-- this block is for dynamically loading links  -->
             <?php
-            $linkSection1 = get_field('link-section1');
-            if ($linkSection1) :
-                // create variables to store the link and the title
-                $link_title = $linkSection1['title'];
-                $link_url = $linkSection1['url'];
-            ?>
-                <button class="btn inv"><a href="<?php print_r(esc_url($link_url)); ?>"><?php print_r(esc_html($link_title)); ?></a></button>
+                $linkAbout = get_field('link-about');
+                if ($linkAbout) :
+                    // create variables to store the link and the title
+                    $link_title = $linkAbout['title'];
+                    $link_url = $linkAbout['url'];
+                ?>
+                    <button class="btn inv"><a href="<?php print_r(esc_url($link_url)); ?>"><?php print_r(esc_html($link_title)); ?></a></button>
             <?php endif; ?>
         </div>
     </section>
 
     <section>
         <div class="max-width">
-            <h2><?php $headingCourses = get_field('heading-courses'); ?> <?php if ($headingCourses) {
-                                                                                _e($headingCourses);
-                                                                            } ?></h2>
+            <h2><?php $headingCourses = get_field('heading-courses'); ?> <?php if ($headingCourses) { _e($headingCourses); } ?></h2>
             <div class="course-container">
                 <?php foreach ($categories as $category) { ?>
 
@@ -153,12 +186,8 @@
 
     <section class="section2">
         <div class="text-container max-width">
-            <h2><?php $headingSection2 = get_field('heading-section2'); ?> <?php if ($headingSection2) {
-                                                                                _e($headingSection2);
-                                                                            } ?></h2>
-            <p><?php $textSection2 = get_field('text-section2'); ?> <?php if ($textSection2) {
-                                                                        _e($textSection2);
-                                                                    } ?></p>
+            <h2><?php $headingSection2 = get_field('heading-section2'); ?> <?php if ($headingSection2) { _e($headingSection2); } ?></h2>
+            <p><?php $textSection2 = get_field('text-section2'); ?> <?php if ($textSection2) {  _e($textSection2); } ?></p>
 
             <!-- this block is for dynamically loading links  -->
             <?php
@@ -175,9 +204,7 @@
 
     <section>
         <div class="max-width">
-            <h2><?php $headingPartners = get_field('heading-partners'); ?> <?php if ($headingPartners) {
-                                                                                _e($headingPartners);
-                                                                            } ?></h2>
+            <h2><?php $headingPartners = get_field('heading-partners'); ?> <?php if ($headingPartners) { _e($headingPartners); } ?></h2>
         </div>
         <div class="course-container partner-container max-width">
             <?php
@@ -194,18 +221,11 @@
             <?php if ($the_query_partners->have_posts()) :  ?>
                 <?php while ($the_query_partners->have_posts()) : $the_query_partners->the_post(); ?>
                     <div class="course card partner-image">
-                        <?php
-
-                        $partnerimage = get_field('partner-image'); ?>
+                        <?php $partnerimage = get_field('partner-image'); ?>
 
                         <?php if ($partnerimage) :  ?>
-
-
                             <a href="<?php echo $partnerimage['link'] ?>"><img src="<?php echo $partnerimage['image']; ?>" /></a>
-
                         <?php endif; ?>
-
-
                     </div>
 
                 <?php endwhile; ?>
